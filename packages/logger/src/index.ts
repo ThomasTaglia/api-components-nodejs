7d6b029aa4fae425b085622ca1186280f100364b
ThomasTaglia <thomastagliaferri@yahoo.com>
2023-07-04 01:13:41 +0200

    fix: add exports on logger

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,7 @@
+export { default as BaseLogInfo } from "./BaseLogInfo";
+export { default as ErrorLogInfo } from "./ErrorLogInfo";
+export { default as Log } from "./Log";
+export { default as LogLevel } from "./LogLevel";
+export { default as Logger } from "./Logger";
+export { default as UserSuppliedLogInfo } from "./UserSuppliedLogInfo";
+export { default as getUnknownErrorDetails } from "./getUnknownErrorDetails";
